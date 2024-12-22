59f0d4cdf17f23ab77f0b39dea597d03fc465690
sushmithacv <154046226+sushmithacv@users.noreply.github.com>
2024-12-22 16:30:47 +0530

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      {children} 
+      
         <DashboardWrapper>{children}</DashboardWrapper>
       </body>
     </html>
